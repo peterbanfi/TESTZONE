c3b23c7d82696ac1fa6ddff2a91daf9c7a903591
unknown <koviko@gamil.com>
2018-04-07 19:00:46 +0200

    minimal move

--- js/class.js
+++ js/class.js
@@ -1,31 +1,3 @@
-/* class item {
-    constructor(height,width){
-        this.height=height;
-        this.width=width;
-    }
-}
-class staticItem extends item {
-    constructor(height,width,type,pos){
-        super(height,width);
-        this.type=type;
-        this.position = this.position;
-    }
-}
-class chicken extends item {
-    constructor(height,width,type){
-        super(height,width);
-        this.type=type;
-        this.position = this.position;
-    }
-}
-class wolf extends item {
-    constructor(height,width,type){
-        super(height,width);
-        this.type=type;
-        this.position = this.position;
-    }
-}
- */
 class chicken {
     constructor(){
         this.data=[];
@@ -41,18 +13,44 @@ class chicken {
         });
     }
     
-    moveOne(i){
-        this.data[i].positionY+=this.data[i].speed;
-        this.data[i].positionX+=this.data[i].speed;
+    moveOne(i,speedX,speedY){
+        this.data[i].positionY+=this.data[i].speed*speedY;
+        this.data[i].positionX+=this.data[i].speed*speedX;
         let x=document.querySelector(`#chicken${i}`);
         x.style.top=chick.data[i].positionY + "px";
         x.style.left=chick.data[i].positionX + "px";
     }
+    moveTillX(i,dif){
+        this.data[i].positionX+=dif;
+        let x=document.querySelector(`#chicken${i}`);
+        x.style.left=chick.data[i].positionX + "px";
+        
+    }
+    moveTillY(i,dif){
+        this.data[i].positionY+=dif;
+        let x=document.querySelector(`#chicken${i}`);
+        x.style.top=chick.data[i].positionY + "px";
+        
+    }
+}
+class Barrier {
+    constructor(){
+        this.data=[];
+    }
+    newBarrier(height,width,type,positionX,positionY){
+        this.data.push({
+            height,
+            width,
+            type,
+            positionX,
+            positionY,
+        });
+    }
 }
 let chick = new chicken();
 chick.newChicken(100,100,1,100,100,50);
-chick.newChicken(100,100,1,200,200,50);
-chick.newChicken(100,100,1,400,400,50);
+/* chick.newChicken(100,100,1,200,200,50);
+chick.newChicken(100,100,1,400,400,50); */
 for(let i in chick.data){
     let x=document.createElement('img');
     x.src='../chicken.png';
@@ -63,7 +61,74 @@ for(let i in chick.data){
     x.id="chicken"+i;
     document.body.appendChild(x);
 }
-function mozog(){
-    chick.moveOne(0);
-    console.log(chick.data[0]);
+let barrier =new Barrier();
+for (let i =0;i<15;i++){
+    barrier.newBarrier(100,100,1,Math.round(Math.random()*1500),Math.round(Math.random()*800));
 }
+
+for(let i in barrier.data){
+    let x=document.createElement('img');
+    x.src='../tree.png';
+    x.style.top=barrier.data[i].positionY + "px";
+    x.style.left=barrier.data[i].positionX + "px";
+    x.style.height=barrier.data[i].height + "px";
+    x.style.width=barrier.data[i].width + "px";
+    x.id="barrier"+i;
+    document.body.appendChild(x);
+}
+function checkMove(foo,speedX,speedY){
+    let newPosX=chick.data[foo].speed*speedX+chick.data[foo].positionX;
+    let newPosY= chick.data[foo].speed*speedY+chick.data[foo].positionY;
+    let h=chick.data[foo].height;
+    let w=chick.data[foo].width;
+    for(let i in barrier.data){
+        
+        if((barrier.data[i].positionX<newPosX || barrier.data[i].positionX<newPosX+w)
+             && (barrier.data[i].positionX+barrier.data[i].width>newPosX  ||barrier.data[i].positionX+barrier.data[i].width>newPosX+w )
+             && (barrier.data[i].positionY<newPosY ||barrier.data[i].positionY<newPosY+h)
+             && (barrier.data[i].positionY+barrier.data[i].height>newPosY ||barrier.data[i].positionY+barrier.data[i].height>newPosY+h)){
+            return speedX==0?barrier.data[i].positionY-newPosY-chick.data[foo].speed*speedY:barrier.data[i].positionX-newPosX-chick.data[foo].speed*speedX;
+        }
+       
+    }
+    return true;
+}
+window.addEventListener('keydown', function (e) {
+    //bal
+    if (e.keyCode == 37){
+        let result =checkMove(0,-1,0);
+        if(result===true){
+            chick.moveOne(0,-1,0);
+        }else{
+            chick.moveTillX(0,result);
+        }
+        
+    }
+    //jobb
+    if (e.keyCode == 39){
+        let result =checkMove(0,1,0);
+        if(result===true){
+            chick.moveOne(0,1,0);
+        }else{
+            chick.moveTillX(0,result);
+        }
+    }
+    //fel
+    if (e.keyCode == 38){
+        let result =checkMove(0,0,-1);
+        if(result===true){
+            chick.moveOne(0,0,-1);
+        }else{
+            chick.moveTillY(0,result);
+        }
+    }
+    //le
+    if (e.keyCode == 40){
+        let result =checkMove(0,0,1);
+        if(result===true){
+            chick.moveOne(0,0,1);
+        }else{
+            chick.moveTillY(0,result);
+        }
+    }
+}, true);
